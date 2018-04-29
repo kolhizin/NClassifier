456646f61c6a66c4370d4ab9e38c1cccedcba4ab
kolhizin <kolhizin@users.noreply.github.com>
2018-04-29 08:44:42 +0300

    Created object selector project in C#

---
+++ apps/ManualImageObjectSelector/ManualImageObjectSelector/SetUp.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace ManualImageObjectSelector
+{
+    partial class SetUp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlSetup1 = new System.Windows.Forms.Panel();
+            this.lblSetup1 = new System.Windows.Forms.Label();
+            this.pnlSetup2 = new System.Windows.Forms.Panel();
+            this.lblSetup2 = new System.Windows.Forms.Label();
+            this.pnlSetup3 = new System.Windows.Forms.Panel();
+            this.lblSetup3 = new System.Windows.Forms.Label();
+            this.pnlSetup1.SuspendLayout();
+            this.pnlSetup2.SuspendLayout();
+            this.pnlSetup3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlSetup1
+            // 
+            this.pnlSetup1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlSetup1.Controls.Add(this.lblSetup1);
+            this.pnlSetup1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlSetup1.Location = new System.Drawing.Point(0, 0);
+            this.pnlSetup1.Name = "pnlSetup1";
+            this.pnlSetup1.Size = new System.Drawing.Size(391, 431);
+            this.pnlSetup1.TabIndex = 0;
+            // 
+            // lblSetup1
+            // 
+            this.lblSetup1.AutoSize = true;
+            this.lblSetup1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblSetup1.Location = new System.Drawing.Point(11, 8);
+            this.lblSetup1.Name = "lblSetup1";
+            this.lblSetup1.Size = new System.Drawing.Size(192, 55);
+            this.lblSetup1.TabIndex = 0;
+            this.lblSetup1.Text = "1. Input";
+            // 
+            // pnlSetup2
+            // 
+            this.pnlSetup2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlSetup2.Controls.Add(this.lblSetup2);
+            this.pnlSetup2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlSetup2.Location = new System.Drawing.Point(391, 0);
+            this.pnlSetup2.Name = "pnlSetup2";
+            this.pnlSetup2.Size = new System.Drawing.Size(353, 253);
+            this.pnlSetup2.TabIndex = 1;
+            // 
+            // lblSetup2
+            // 
+            this.lblSetup2.AutoSize = true;
+            this.lblSetup2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblSetup2.Location = new System.Drawing.Point(11, 8);
+            this.lblSetup2.Name = "lblSetup2";
+            this.lblSetup2.Size = new System.Drawing.Size(230, 55);
+            this.lblSetup2.TabIndex = 0;
+            this.lblSetup2.Text = "2. Output";
+            // 
+            // pnlSetup3
+            // 
+            this.pnlSetup3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlSetup3.Controls.Add(this.lblSetup3);
+            this.pnlSetup3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlSetup3.Location = new System.Drawing.Point(391, 253);
+            this.pnlSetup3.Name = "pnlSetup3";
+            this.pnlSetup3.Size = new System.Drawing.Size(353, 178);
+            this.pnlSetup3.TabIndex = 2;
+            // 
+            // lblSetup3
+            // 
+            this.lblSetup3.AutoSize = true;
+            this.lblSetup3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblSetup3.Location = new System.Drawing.Point(11, 8);
+            this.lblSetup3.Name = "lblSetup3";
+            this.lblSetup3.Size = new System.Drawing.Size(213, 55);
+            this.lblSetup3.TabIndex = 0;
+            this.lblSetup3.Text = "3. Finish";
+            // 
+            // SetUp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(744, 431);
+            this.Controls.Add(this.pnlSetup2);
+            this.Controls.Add(this.pnlSetup3);
+            this.Controls.Add(this.pnlSetup1);
+            this.Name = "SetUp";
+            this.Text = "Form1";
+            this.pnlSetup1.ResumeLayout(false);
+            this.pnlSetup1.PerformLayout();
+            this.pnlSetup2.ResumeLayout(false);
+            this.pnlSetup2.PerformLayout();
+            this.pnlSetup3.ResumeLayout(false);
+            this.pnlSetup3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlSetup1;
+        private System.Windows.Forms.Label lblSetup1;
+        private System.Windows.Forms.Panel pnlSetup2;
+        private System.Windows.Forms.Label lblSetup2;
+        private System.Windows.Forms.Panel pnlSetup3;
+        private System.Windows.Forms.Label lblSetup3;
+    }
+}
+
