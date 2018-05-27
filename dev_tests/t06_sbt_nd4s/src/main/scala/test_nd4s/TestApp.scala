38dd518b71d03d8e621349340b3e4592d2833da5
kolhizin <kolhizin@gmail.com>
2018-05-27 20:46:17 +0300

    tested nd4s

--- dev_tests/t06_sbt_nd4s/src/main/scala/test_nd4s/TestApp.scala
+++ dev_tests/t06_sbt_nd4s/src/main/scala/test_nd4s/TestApp.scala
@@ -4,7 +4,9 @@ import org.nd4j.linalg.factory.Nd4j
 import org.nd4s.Implicits._
 
 object TestApp extends App {
-  println(Nd4j.ones(3,3,3))
-
+  val tmp1 = Nd4j.ones(3,3)
+  val tmp2 = (1 to 3).asNDArray(3)
+  val tmp3 = tmp1 + tmp2.broadcast(3,3)
+  println(tmp3)
   println("Done")
 }
